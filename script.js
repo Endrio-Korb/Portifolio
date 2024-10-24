ae7b41378423b02b8070f418c51196ee676cfde6
Endrio-Korb <alexkorb95@gmail.com>
2024-10-24 20:01:17 -0300

    reduzido função de ver mais projetos

--- script.js
+++ script.js
@@ -2,21 +2,16 @@ var ver_mais = document.getElementById("ver-mais")
 const projetos = document.querySelectorAll(".projetos-ocultos")
 
 function verMais(e) {
-    if (ver_mais.innerHTML == "Ver mais") {
-        ver_mais.textContent = "Ver menos";
-        for (let i = 0; i < projetos.length; i++) {
-            projetos[i].setAttribute('class', 'grid-item projetos projetos-ativos')
-
-        }
-    } else {
-        ver_mais.textContent = "Ver mais";
-        for (let i = 0; i < projetos.length; i++) {
-            projetos[i].setAttribute('class', 'projetos-ocultos')
-
-        }
-    }
+    
+    projetos.forEach ( (projeto) => {
+        projeto.classList.toggle("projetos-ocultos")
+    })
 }
 
+
+
+
+const elementos = document.querySelectorAll(".hidden")
 
 
 const myObserver = new IntersectionObserver( (entries) => {
@@ -28,7 +23,6 @@ const myObserver = new IntersectionObserver( (entries) => {
         }
     })
 })
-
-const elementos = document.querySelectorAll(".hidden")
-
 elementos.forEach( (elemento) => myObserver.observe(elemento))
+
+
